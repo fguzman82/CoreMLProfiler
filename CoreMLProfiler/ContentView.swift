f91e76f0dd6a01b28960f8d05ad386f84f644dd6
fabio <fabio.guzmanf@upb.edu.co>
2024-06-08 13:35:30 -0500

    Initial Commit

---
+++ CoreMLProfiler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreMLProfiler
+//
+//  Created by Fabio Guzman on 8/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreMLProfiler/CoreMLProfilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLProfilerApp.swift
+//  CoreMLProfiler
+//
+//  Created by Fabio Guzman on 8/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLProfilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
